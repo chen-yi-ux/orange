f9baf69d51ee54196cb5ebad00f1b9fc1d038724
Cyer <1678447359@qq.com>
2021-08-23 23:08:03 +0800

    重复两次布局

--- src/views/Chart.vue
+++ src/views/Chart.vue
@@ -1,8 +1,10 @@
 <template>
-<div>
-  Chart.vue
-  <Nav/>
-</div>
+  <div class="nav-wrapper">
+    <div class="chart">
+      <p>Chart.vue</p>
+    </div>
+    <Nav/>
+  </div>
 </template>
 
 <script lang="ts">
@@ -15,5 +17,15 @@ export default class Chart extends Vue {
 </script>
 
 <style lang="scss" scoped>
-
+.nav-wrapper{
+  display: flex;
+  border: 1px solid green;
+  flex-direction: column;
+  height: 100vh;
+}
+.chart{
+  border: 1px solid blue;
+  flex-grow: 1;       // 尽量把所有高度都给 .chart
+  overflow: auto;      // 当这个元素内容太大，显示滚动条
+}
 </style>
