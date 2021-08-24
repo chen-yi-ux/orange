c7cf2cb377ece832e48a5a32a6caf262df538540
Cyer <1678447359@qq.com>
2021-08-24 11:02:01 +0800

    正确引入 SVG，创建 <Icon/>，并全局引用

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="nav">
-    <router-link to="/detail">明细</router-link>
+    <router-link to="/detail">
+      <Icon name="detail"/>
+      明细
+    </router-link>
     |
     <router-link to="/money">记一笔</router-link>
     |
-    <router-link to="/chart">图表</router-link>
+    <router-link to="/chart">
+      <Icon name="chart"/>
+      图表
+    </router-link>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
+
 
 @Component
 export default class Nav extends Vue {
@@ -19,7 +26,7 @@ export default class Nav extends Vue {
 </script>
 
 <style lang="scss" scoped>
-.nav{
+.nav {
   border: 1px solid red;
 }
 </style>
