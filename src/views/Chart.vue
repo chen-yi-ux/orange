52fa400af2aa588a46ba5b0fff4cafa7942848dd
Cyer <1678447359@qq.com>
2021-08-24 09:48:00 +0800

    全局使用布局组件，利用插槽 <slot/>

--- src/views/Chart.vue
+++ src/views/Chart.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="chart">
-      <p>Chart.vue</p>
-    </div>
+  <div>
+    <Layout>
+      Chart.vue
+    </Layout>
     <Nav/>
   </div>
 </template>
@@ -10,6 +10,7 @@
 <script lang="ts">
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
+
 @Component
 export default class Chart extends Vue {
 
@@ -17,15 +18,5 @@ export default class Chart extends Vue {
 </script>
 
 <style lang="scss" scoped>
-.nav-wrapper{
-  display: flex;
-  border: 1px solid green;
-  flex-direction: column;
-  height: 100vh;
-}
-.chart{
-  border: 1px solid blue;
-  flex-grow: 1;       // 尽量把所有高度都给 .chart
-  overflow: auto;      // 当这个元素内容太大，显示滚动条
-}
+
 </style>
--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,15 +1,15 @@
 <template>
-<div class="nav-wrapper">
-  <div class="detail">
-    <p>Detail.vue</p>
+  <div>
+    <Layout>
+      Detail.vue
+    </Layout>
   </div>
-  <Nav/>
-</div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
+
 @Component
 export default class Detail extends Vue {
 
@@ -17,15 +17,5 @@ export default class Detail extends Vue {
 </script>
 
 <style lang="scss" scoped>
-.nav-wrapper{
-  display: flex;
-  border: 1px solid green;
-  flex-direction: column;
-  height: 100vh;
-}
-.detail{
-  border: 1px solid blue;
-  flex-grow: 1;       // 尽量把所有高度都给 .detail
-  overflow: auto;      // 当这个元素内容太大，显示滚动条
-}
+
 </style>
