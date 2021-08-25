df77b2b184d16f1cc3d6d44b933f919a421e7da8
Cyer <1678447359@qq.com>
2021-08-25 15:59:11 +0800

    添加了 Notes 组件

--- src/components/Money/Notes.vue
+++ src/components/Money/Notes.vue
@@ -1,8 +1,8 @@
 <template>
-<div class="time">
-  <Icon name="time" class="timeIcon"/>
-  <span class="content">时间</span>
-  <input type="text" placeholder="记录时间">
+<div class="notes">
+  <Icon name="note" class="noteIcon"/>
+  <span class="content">备注</span>
+  <input type="text" placeholder="...">
 </div>
 </template>
 
@@ -11,19 +11,19 @@ import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 
 @Component
-export default class Time extends Vue {
+export default class Notes extends Vue {
 
 }
 </script>
 
 <style lang="scss" scoped>
-.time{
+.notes{
   font-size: 18px;
   padding: 13px 0;
   border-bottom: 1px solid #e3e3e3;
   display: flex;
   align-items: center;
-  > .timeIcon{
+  > .noteIcon{
     width: 30px;
     height: 30px;
   }
@@ -33,10 +33,11 @@ export default class Time extends Vue {
     color: #636363;
   }
   input{
-
-    //background:none;
-    //outline:none;
-    //border:0;
+    background:none;
+    outline:none;
+    border:0;
+    color: black;
+    font-size: 20px;
   }
 }
 </style>
