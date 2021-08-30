5770e2ad8fc59375f78979bb13a836c171718304
Cyer <1678447359@qq.com>
2021-08-30 19:39:47 +0800

    删除 Save 组件，更改 Notes 的触发方式

--- src/components/Money/Notes.vue
+++ src/components/Money/Notes.vue
@@ -1,11 +1,11 @@
 <template>
-<div class="notes">
-  <Icon name="note" class="noteIcon"/>
-  <span class="content">备注</span>
-  <input type="text"
-         v-model="this.value"
-         placeholder="...">
-</div>
+  <div class="notes">
+    <Icon name="note" class="noteIcon"/>
+    <span class="content">备注</span>
+    <input type="text"
+           v-model="value"
+           placeholder="...">
+  </div>
 </template>
 
 <script lang="ts">
@@ -14,34 +14,38 @@ import {Component, Prop, Watch} from 'vue-property-decorator';
 
 @Component
 export default class Notes extends Vue {
-  @Prop() value: string | undefined;
+  value = '';
+
   @Watch('value')
-  onValueChanged(value: string){
-    this.$emit('update:value', value)
+  onValueChanged(value: string) {
+    this.$emit('update:value', value);
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.notes{
+.notes {
   font-size: 18px;
   padding: 13px 0;
   border-bottom: 1px solid #e3e3e3;
   display: flex;
   align-items: center;
-  > .noteIcon{
+
+  > .noteIcon {
     width: 27px;
     height: 27px;
   }
-  > .content{
+
+  > .content {
     padding-left: 5px;
     padding-right: 18px;
     color: #636363;
   }
-  input{
-    background:none;
-    outline:none;
-    border:0;
+
+  input {
+    background: none;
+    outline: none;
+    border: 0;
     color: black;
     font-size: 20px;
   }
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -9,13 +9,15 @@
         <div class="block">
           <Input :value.sync="record.amount"/>
           <Time @update:value="record.date = $event"/>
-          <Notes :value.sync="record.notes"/>
+          <Notes @update:value="record.notes = $event"/>
           <Labels :type="record.type"
                   :selected-labels.sync="record.labels"
-                  />
+          />
         </div>
         <div class="save">
-          <Save/>
+          <button @click="saveRecord">
+            保存
+          </button>
         </div>
       </div>
     </main>
@@ -24,14 +26,13 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import {Component} from 'vue-property-decorator';
+import {Component, Watch} from 'vue-property-decorator';
 import Title from '@/components/Money/Title.vue';
 import Type from '@/components/Money/Type.vue';
 import Input from '@/components/Money/Input.vue';
 import Time from '@/components/Money/Time.vue';
 import Notes from '@/components/Money/Notes.vue';
 import Labels from '@/components/Money/Labels.vue';
-import Save from '@/components/Money/Save.vue';
 import {Label} from '@/custom';
 
 type Record = {
@@ -43,10 +44,22 @@ type Record = {
 }
 
 @Component({
-  components: {Save, Labels, Notes, Time, Input, Type, Title}
+  components: {Labels, Notes, Time, Input, Type, Title}
 })
 export default class Money extends Vue {
   record: Record = {type: '-', amount: '0.00', date: '', notes: '', labels: {name: '三餐', svg: '三餐', type: '-'}};
+  recordList: Record[] = [];
+
+  saveRecord() {
+    const record2 = JSON.parse(JSON.stringify(this.record));
+    this.recordList.push(record2);
+    console.log(this.recordList);
+  }
+
+  @Watch('recordList')
+  onRecordListChanged() {
+    window.localStorage.setItem('recordList', JSON.stringify(this.recordList));
+  }
 
 }
 </script>
@@ -79,6 +92,16 @@ main {
       display: flex;
       justify-content: center;
       align-items: center;
+
+      > button {
+        width: 35%;
+        height: 80%;
+        background: $color-highlight;
+        color: white;
+        font-size: 24px;
+        border-radius: 35px;
+        border: none;
+      }
     }
   }
 }
